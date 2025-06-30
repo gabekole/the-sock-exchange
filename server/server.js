89d1eb95905b993c4c05862c5a194a59ce0d1949
wasadmin <wasadmin@example.com>
2025-06-29 21:18:34 -0400

    working getroute

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); // Enable CORS for all routes
+
 // Endpoint to read and send JSON file content
 app.get('/socks', async (_req, res) => {
     try {
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        res.json(jsonObj);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
